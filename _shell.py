58e3b4701970fc94b7ca26151f1b7c9d205a1fe5
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-07-11 11:31:12 -0400

    I was obviously tired (0.7.4)
    (Late) Last night I forgot to change the version from 0.6.1 to 0.7
    Fixed it
    I am so dumb :smile:

--- _shell.py
+++ _shell.py
@@ -29,7 +29,7 @@ def ls():
     clear
     ''')
 def version(): #make a new command called version()
-    print("Welcome to CustPy 0.6.5! thanks for using! Finally gamma!!!! ") # output all that
+    print("Welcome to CustPy 0.7.4! thanks for using! Finally gamma!!!! ") # output all that
 def Shell(): #define command Shell()
     while True: # do all this section in a loop (when done, restart)
         txt = input("Last Login: Unknown. !") #Add the prompt
@@ -40,7 +40,7 @@ def Shell(): #define command Shell()
         elif txt == "Sing this song to You":
             print("It goes like this: The Fourth The Fifth...")
         elif txt == "version":
-            print("This is CustPy Shell 0.6.5. Thanks for using!")
+            print("This is CustPy Shell 0.7.4. Thanks for using!")
         elif txt == "ls": #if they type ls
             ls() # run the newly-made ls() command (lines 9 through 16)
         elif txt == "ver":
