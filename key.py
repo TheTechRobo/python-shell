4c43bf07c76a4c82e5363c538d7f05233a9ad0e3
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-07-23 10:37:58 -0400

    Add files via upload
    for the activ() command

---
+++ key.py
@@ -0,0 +1,15 @@
+activatedItem1 = "cls"
+activatedItem2 = "Slate"
+whoa223 = 11000001110
+loHe = whoa223 + 1
+dds330 = loHe
+key = dds330
+dds330 = dds330 + 10
+inp = input("Please enter your key...")
+if inp == dds330 + key:
+    print("you found the key!")
+    print("You have activated the following commands:")
+    print("the " +acticatedItem2)
+    print("and " +activatedItem1)
+else: 
+    print("WRONG!")
