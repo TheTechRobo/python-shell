1f76b9629943340cdd20e10dd12f5578eb61d9b7
TtR <52163910+TheTechRobo@users.noreply.github.com>
2019-07-29 21:11:16 -0400

    Update key.py
    • changed key
    • cleaned up
    • reorganized

--- key.py
+++ key.py
@@ -1,15 +1,16 @@
 activatedItem1 = "cls"
-activatedItem2 = "Slate"
+activatedItem2 = "slate"
 whoa223 = 11000001101
 loHe = whoa223 + 1
 dds330 = loHe
 key = dds330
-dds330 = dds330 + 10
+dds330 = dds330 + 11.1
+key = dds330
 inp = input("Please enter your key...")
 if inp == dds330 + key:
     print("you found the key!")
     print("You have activated the following commands:")
-    print("the " +acticatedItem2)
-    print("and " +activatedItem1)
+    print(activatedItem1)
+    print(acticatedItem2)
 else: 
     print("WRONG!")
