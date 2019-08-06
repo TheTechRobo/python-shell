e63db649b7ce8c988b1d854a5640f189e683812b
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2019-08-05 21:51:09 -0400

    Create activ.sh
    • created
    • bumped version

---
+++ activ.sh
@@ -0,0 +1,4 @@
+echo Loading activ:mac 0.1
+echo Hiya! I'm loading the key.py file — just for you. Make sure it's in the same folder as _shell.py
+echo Assuming that Python 3.x is installed on your machine...this should work
+python3 key.py
